b3deda3dfb5f0a09cf976a2af3eaf317877de4db
UnTamed Fury <168332215+UnTamed-Fury@users.noreply.github.com>
2026-04-22 02:35:46 +0530

    fix: syntax error in AccountType enum

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/game/account/AccountType.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/game/account/AccountType.kt
@@ -19,5 +19,5 @@
 package com.movtery.zalithlauncher.game.account
 
 enum class AccountType(val tag: String) {
-    MICROSOFT(\"Microsoft\"), LOCAL(\"Local\")
+    MICROSOFT("Microsoft"), LOCAL("Local")
 }
